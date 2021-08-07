7065143a703ce406453a364e06fea455216df214
Arslan <cha69895@gmail.com>
2021-08-07 23:49:26 +0500

    hanges to be committed:

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,12 @@ import {Link} from 'react-router-dom'
 function Hero() {
     return (
         <div className='rounded  bg-gray-900'>
-        <div className='justify-center bg-white h-screen flex items-center flex-col '>
+        <div className='justify-center bg-white h-screen flex items-center  flex-col '>
+            <div className=' border-solid animate-ping rounded-xl  outline-black'>
+            {/* <Link className='animate-none  an'><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+</svg></Link> */}
+</div>
             <h1 className='xl:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14' >Mongo</h1>
             <Link className='py-7 px-5 bg-yellow-200 rounded-full text-3xl hover:bg-yellow-700 
             animate-bounce transition-douration-300 flex items-center'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import logoimg from'../images/logo.jpg';
 import {Link} from 'react-router-dom'
  function Navbar() {
     return (
-      <nav className='flex justify-around items-stretch   relative font-mono bg-blue-600  h-16 py-5 '>
-       <Link to='/' className='pl-4 text-xl'>
+      <nav className='flex justify-around items-stretch   relative font-mono bg-blue-400   h-16 py-5 '>
+       <Link to='/' className='pl-4  flex text-2xl'>
          <img src={logoimg} alt="logo"/>
        Egg
        </Link>
@@ -14,7 +14,7 @@ import {Link} from 'react-router-dom'
   2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
        </div>
      
-    <div className=' pr-8 md:block hidden '>
+    <div className=' pr-8 md:block hidden text-xl '>
      <Link to='/' className='p-4 '>Home</Link>
      <Link to='/' className='p-4 '>About</Link>
      <Link to='/' className='p-4 '>Contact</Link>
